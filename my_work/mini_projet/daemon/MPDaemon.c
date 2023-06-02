4200548b31f801f354ac79070d6cc80bb0dbb775
Rod_lenovo_linux <quentin.rod@master.hes-so.ch>
2023-06-02 18:20:46 +0200

    add screen files

--- my_work/mini_projet/daemon/MPDaemon.c
+++ my_work/mini_projet/daemon/MPDaemon.c
@@ -36,6 +36,7 @@
 #include <stdio.h>
 #include <sys/timerfd.h>
 #include <syslog.h>
+#include "ssd1306.h"
 
 #define GPIO_EXPORT   "/sys/class/gpio/export"
 #define GPIO_UNEXPORT "/sys/class/gpio/unexport"
@@ -188,6 +189,23 @@ static int open_cooling_controller(int* fd){
 }
 int main(int argc, char* argv[])
 {
+    ssd1306_init();
+
+    ssd1306_set_position (0,0);
+    ssd1306_puts("CSEL1a - SP.07");
+    ssd1306_set_position (0,1);
+    ssd1306_puts("  Demo - SW");
+    ssd1306_set_position (0,2);
+    ssd1306_puts("--------------");
+
+    ssd1306_set_position (0,3);
+    ssd1306_puts("Temp: 35'C");
+    ssd1306_set_position (0,4);
+    ssd1306_puts("Freq: 1Hz");
+    ssd1306_set_position (0,5);
+    ssd1306_puts("Duty: 50%");
+    return 0;
+
     //openlog("csel_syslog", LOG_PID, LOG_LOCAL3); //Void function
     int fd_cooling[NB_COOLING_CONTROLLER_ATTR]; 
     int ret = open_cooling_controller(fd_cooling);
