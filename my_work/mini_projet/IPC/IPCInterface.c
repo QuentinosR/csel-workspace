486f2a2286bee6a9659e3ce36e24a13474f0ab1b
Rod_lenovo_linux <quentin.rod@master.hes-so.ch>
2023-06-07 21:04:33 +0200

    add IPC files

---
+++ my_work/mini_projet/IPC/IPCInterface.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define FIFO_PATH "/tmp/MPFifo"
+
+int main() {
+    int fd;
+    char command[100];
+
+    // Open the named pipe for reading
+    fd = open(FIFO_PATH, O_WRONLY);
+    if (fd == -1) {
+        perror("open");
+        exit(1);
+    }
+
+    printf("\"stop\" command to exit\n");
+
+    while(1){
+        printf("[MPIPC]# ");
+        fgets(command, sizeof(command), stdin);
+        command[strlen(command)-1] = '\0'; // Remove newline character from the input
+        if(strcmp(command, "stop") == 0)
+            break;
+        write(fd, command, strlen(command));
+        printf("Writer: Message sent: %s\n", command);
+    }
+
+    // Close the named pipe
+    close(fd);
+    // Remove the named pipe
+    unlink(FIFO_PATH);
+
+    return 0;
+}
